83d0237ec3b188fd59125672a5686dbe03e8a822
Ross Golder <ross@golder.org>
2026-09-11 18:04:58 +0700

    Migrate to Crossplane v2 .m. API group
    - Update v1beta1 API group from gitea.crossplane.io to gitea.m.crossplane.io
    - Use namespaced resources for multi-tenant isolation

--- apis/v1beta1/groupversion_info.go
+++ apis/v1beta1/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1beta1 contains the v1beta1 group gitea.crossplane.io resources of the provider.
+// Package v1beta1 contains the v1beta1 group gitea.m.crossplane.io resources of the provider.
 // +kubebuilder:object:generate=true
-// +groupName=gitea.crossplane.io
+// +groupName=gitea.m.crossplane.io
 // +versionName=v1beta1
 package v1beta1
 
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	Group   = "gitea.crossplane.io"
+	Group   = "gitea.m.crossplane.io"
 	Version = "v1beta1"
 )
 
